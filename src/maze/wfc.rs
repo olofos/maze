705fd9401af89eaaaba321ceafbe69ec9d4bb408
Olof Ohlsson Sax <olof.ohlsson.sax@gmail.com>
2024-09-19 14:19:25 +0200

    Refactoring of wfc::step

--- src/maze/wfc.rs
+++ src/maze/wfc.rs
@@ -15,42 +15,42 @@ pub fn init() -> MazeState {
 }
 
 pub fn step(state: &mut MazeState, grid: &mut Grid) {
-    let mut moves = Vec::new();
+    let mut tiles = Vec::new();
+
+    let mut min_len = 4;
     for y in 0..GRID_HEIGHT {
         for x in 0..GRID_WIDTH {
-            moves.push(grid.possible_moves(IVec2::new(x as i32, y as i32)));
+            let i = y * GRID_WIDTH + x;
+            let m = grid.possible_moves(IVec2::new(x as i32, y as i32));
+
+            if !state.fixed[i] && !m.is_empty() && m.len() <= min_len {
+                min_len = min_len.min(m.len());
+                tiles.push((i, m));
+            }
         }
     }
+    tiles.retain(|(_, v)| v.len() == min_len);
 
-    let mut rng = rand::thread_rng();
-
-    let mut cells = moves
-        .iter()
-        .enumerate()
-        .filter(|(i, v)| !state.fixed[*i] && !v.is_empty())
-        .collect::<Vec<_>>();
-
-    if cells.is_empty() {
-        println!("Cells is empty. Retrying...");
+    if tiles.is_empty() {
+        println!("Tile list is empty. Retrying...");
         state.fixed.fill(false);
         return;
     }
 
-    cells.sort_by_key(|(_, v)| v.len());
-    let min_len = cells[0].1.len();
-    cells.retain(|(_, v)| v.len() == min_len);
-    let cells_index = rng.gen_range(0..cells.len());
-    let (min_index, min_moves) = cells[cells_index];
+    let mut rng = rand::thread_rng();
+
+    let tile_index = rng.gen_range(0..tiles.len());
+    let (min_index, min_moves) = &tiles[tile_index];
 
     let min_y = min_index / GRID_WIDTH;
     let min_x = min_index % GRID_WIDTH;
     let pos = IVec2::new(min_x as i32, min_y as i32);
 
-    for mov in min_moves {
+    for dir in min_moves {
         if rng.gen_bool(0.5) {
-            let _ = grid.remove_wall(pos, *mov);
+            let _ = grid.remove_wall(pos, *dir);
         }
     }
 
-    state.fixed[min_index] = true;
+    state.fixed[*min_index] = true;
 }
